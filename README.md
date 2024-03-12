c0b89a3c8161ff0c9a5c9a2205143483271d0380
Hsabonchi <developersabon@gmail.com>
2024-03-12 13:38:33 -0500

    add run method to print contents

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Demo little-Treasures
 - Create & Add `hotels.yml`file and place it under `/src/main/resources`
 - Add spring.config.import=hotels.yml to the application.properties file.-
 - create Hotel and HotelProperties as entity classes.
- - Ensure to annotate HotelProperties with `@Component`` to allow detection by the application context."
+ - Ensure to annotate HotelProperties with `@Component`` to allow detection by the  application context."
+ - Ensure to add `@ConfigurationProperties(prefix = "treasures")`to HotelProperties
+      - `@ConfigurationProperties` Annotation for externalized configuration. Add this to a class definition or a @Bean method in a @Configuration class if you want to bind and validate some external Properties (e.g. from a .properties file)
--- littletreasures/src/main/java/com/example/littletreasures/LittletreasuresApplication.java
+++ littletreasures/src/main/java/com/example/littletreasures/LittletreasuresApplication.java
@@ -1,13 +1,36 @@
 package com.example.littletreasures;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LittletreasuresApplication {
+import com.example.littletreasures.entity.Hotel;
+import com.example.littletreasures.entity.HotelProperties;
 
+@SpringBootApplication
+public class LittletreasuresApplication implements CommandLineRunner {
+	@Autowired
+	HotelProperties hotelProperties;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LittletreasuresApplication.class, args);
 	}
+	
+	@Override
+    public void run(String... args) throws Exception {
+        // Access the values from the YAML file
+        int fileVersion = hotelProperties.getFileVersion();
+        List<Hotel> hotels = hotelProperties.getHotels();
+
+     
+        for (Hotel hotel : hotels) {
+            System.out.println("Hotel Name: " + hotel.getName());
+            System.out.println("Hotel Address: " + hotel.getAddress());
+            // Print other hotel properties
+        }
+    }
 
 }
