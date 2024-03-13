9c0f53dcffed04a70a62cb52907c8b42e1d47d89
Hsabonchi <developersabon@gmail.com>
2024-03-13 00:15:46 -0500

    update run methods

--- littletreasures/src/main/java/com/example/littletreasures/LittletreasuresApplication.java
+++ littletreasures/src/main/java/com/example/littletreasures/LittletreasuresApplication.java
@@ -1,14 +1,12 @@
 package com.example.littletreasures;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.littletreasures.entity.Hotel;
 import com.example.littletreasures.entity.HotelProperties;
+import com.example.littletreasures.service.YmlService;
 
 @SpringBootApplication
 public class LittletreasuresApplication implements CommandLineRunner {
@@ -19,18 +17,13 @@ public class LittletreasuresApplication implements CommandLineRunner {
 		SpringApplication.run(LittletreasuresApplication.class, args);
 	}
 	
-	@Override
-    public void run(String... args) throws Exception {
-        // Access the values from the YAML file
-        int fileVersion = hotelProperties.getFileVersion();
-        List<Hotel> hotels = hotelProperties.getHotels();
 
-     
-        for (Hotel hotel : hotels) {
-            System.out.println("Hotel Name: " + hotel.getName());
-            System.out.println("Hotel Address: " + hotel.getAddress());
-            // Print other hotel properties
-        }
-    }
+	@Autowired
+	YmlService ymlService;
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("Version" + ymlService.getHotels());
+	}
 
 }
