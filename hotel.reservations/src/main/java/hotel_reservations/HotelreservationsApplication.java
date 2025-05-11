2e8e49bfb81c0f9cf9e5303a2b2c7194e9b65525
Hsabonchi <developersabon@gmail.com>
2025-05-10 19:16:54 -0500

    create endpoint

---
+++ hotel.reservations/src/main/java/hotel_reservations/HotelreservationsApplication.java
@@ -0,0 +1,13 @@
+package hotel_reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelreservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelreservationsApplication.class, args);
+	}
+
+}
---
+++ hotel.reservations/src/test/java/hotel_reservations/HotelreservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package hotel_reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelreservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
